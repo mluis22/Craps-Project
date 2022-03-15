a28b6cc92b601e6b83c3595dc282be94aa34edf9
mluis22 <97562135+mluis22@users.noreply.github.com>
2022-03-14 21:20:54 -0700

    dice class: includes two dice generated using math.random

---
+++ dice.java
@@ -0,0 +1,15 @@
+public class dice{
+
+	public int diceRollOne(){
+		int n1 = (int) ((Math.random()*6)+1);
+		return n1;
+	}
+
+	public int diceRollTwo(){
+		int n2 = (int) ((Math.random()*6)+1);
+		return n2;
+	}
+
+
+
+}
